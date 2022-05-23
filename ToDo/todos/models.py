e6bec4bf46f6afb021eb26335d4d2b751b85675e
MojtabaMonfared <mojmon42@gmail.com>
2022-05-24 02:54:45 +0430

    create todo app basics like add and delete tasks

--- ToDo/todos/models.py
+++ ToDo/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=False)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField(default=0)
+    status = models.BooleanField(name="Done?", default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "tasks"
+        ordering = ["-priority"]
--- ToDo/todos/views.py
+++ ToDo/todos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import TaskForm
+
+from .models import Tasks
+
+def Home(request):
+    tasks = Tasks.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        if request.POST.get('add_task'):
+            form = TaskForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        if request.POST.get('delete_task'):
+            task = Tasks.objects.get(id=request.POST.get('task_id'))
+            task.delete()
+            return redirect('/')
+            
+    return render(request, 'todos/index.html', {'tasks': tasks, 'form': form})
